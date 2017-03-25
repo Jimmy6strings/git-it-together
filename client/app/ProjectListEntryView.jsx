08d610889df8d1ec387d81bd1e955383e4d5d60a
James Mitchell <mitchell.thehumantornado@gmail.com>
2017-03-25 16:13:57 -0500

    modified comments

--- client/app/ProjectListEntryView.jsx
+++ client/app/ProjectListEntryView.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ProjectList from './ProjectList.jsx';
 
-{/*render data for each project in project list. takes in project and click event for each project*/}
+//render data for each project in project list. takes in project and click event for each project
 var ProjectListEntry = ({project, handleProjectListEntryClick}) => (
-  {/*enters a project with click on project*/}
+  //enters a project with click on project
   <div className="project-list-entry" onClick={() => handleProjectListEntryClick(project)}>
     <div className="project-body">
       <div className="row">
